cb9f68ccdafe09e834953ee44cabe6bece4e8610
Rob <rkeplin@gmail.com>
2019-08-26 15:38:26 -0400

    Some minor adjustments with how things are displayed

--- index.php
+++ index.php
@@ -7,13 +7,18 @@
         <?php $categories = get_the_category(); ?>
         <div class="pb25 pt-2 <?php echo ($i % 2 == 1) ? 'bg-light' : 'bg-gray'; ?>">
             <div class="container">
-                <h1 class="display-4 mb25"><span class="hash">#</span> <a class="text-dark" title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-                <?php foreach ($categories as $category) : ?>
-                    <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
-                <?php endforeach; ?>
-                <div class="post-content">
-                    <p><?php the_excerpt() ?></p>
-                    <a title="Continue Reading" class="btn btn-primary" href="<?php the_permalink() ?>">Read More</a>
+                <div class="wide">
+                    <h1 class="display-4 mb25"><span class="hash">#</span> <a class="text-dark" title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+                    <div>
+                        Category:
+                        <?php foreach ($categories as $category) : ?>
+                            <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="post-content">
+                        <p><?php the_excerpt() ?></p>
+                        <a title="Continue Reading" class="btn btn-primary" href="<?php the_permalink() ?>">Read More</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,7 +33,10 @@
     ?>
 <?php else : ?>
     <div class="container text-center">
-        <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+        <div class="wide">
+            <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+        </div>
+
         <p>Whoops! The page that you were looking for, could not be found.</p>
         <p>Perhaps it got moved to another location, or no longer exists.</p>
         <a title="Back to the blog" class="btn btn-primary" href="<?php echo get_permalink(get_page_by_path('blog')); ?>">Back to the blog</a>
--- single.php
+++ single.php
@@ -4,12 +4,16 @@
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
             <?php $categories = get_the_category(); ?>
-            <h1 class="display-4 mb25"><span class="hash">#</span> <?php the_title(); ?></h1>
-            <?php foreach ($categories as $category) : ?>
-                <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
-            <?php endforeach; ?>
+            <div class="wide">
+                <h1 class="display-4 mb25"><span class="hash">#</span> <?php the_title(); ?></h1>
+                <div class="text-center">
+                    <?php foreach ($categories as $category) : ?>
+                        <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
+                    <?php endforeach; ?>
+                </div>
 
-        <?php the_post_thumbnail('large-thumbnail', array('class' => '')); ?>
+                <?php the_post_thumbnail('large-thumbnail', array('class' => '')); ?>
+            </div>
         <div class="post-content mt-4">
             <?php the_content() ?>
         </div>
