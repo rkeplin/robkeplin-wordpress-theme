3e665cf61278d676aeca9b43913da1323fdc6aa4
Rob <rkeplin@gmail.com>
2019-08-27 11:44:02 -0400

    Updated styles

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <?php while ( have_posts() ) : the_post(); ?>
         <?php $i++; ?>
         <?php $categories = get_the_category(); ?>
-        <div class="pb25 pt-2 <?php echo ($i % 2 == 1) ? 'bg-light' : 'bg-gray'; ?>">
+        <div class="<?php echo ($i % 2 == 1) ? 'bg-light' : 'bg-white'; ?>">
             <div class="container">
-                <div class="wide">
+                <div class="p-4 wide">
                     <h1 class="display-4 mb25"><span class="hash">#</span> <a class="text-dark" title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
                     <div>
                         Category:
@@ -32,14 +32,15 @@
     ));
     ?>
 <?php else : ?>
-    <div class="container text-center">
-        <div class="wide">
-            <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+    <div class="text-center p-5 bg-yellow" style="margin-top: -15px;">
+        <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+    </div>
+    <div class="container">
+        <div class="post-content mt-4 text-center">
+            <p>Whoops! The page that you were looking for, could not be found.</p>
+            <p>Perhaps it got moved to another location, or no longer exists.</p>
+            <a title="Back to the blog" class="btn btn-primary" href="<?php echo get_permalink(get_page_by_path('blog')); ?>">Back to the blog</a>
         </div>
-
-        <p>Whoops! The page that you were looking for, could not be found.</p>
-        <p>Perhaps it got moved to another location, or no longer exists.</p>
-        <a title="Back to the blog" class="btn btn-primary" href="<?php echo get_permalink(get_page_by_path('blog')); ?>">Back to the blog</a>
     </div>
 <?php endif; ?>
 
--- single.php
+++ single.php
@@ -1,38 +1,42 @@
 <?php get_header(); ?>
 
-<div class="container pt-2">
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <?php $categories = get_the_category(); ?>
-            <div class="wide">
-                <h1 class="display-4 mb25"><span class="hash">#</span> <?php the_title(); ?></h1>
-                <div>
-                    Category:
-                    <?php foreach ($categories as $category) : ?>
-                        <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
-                    <?php endforeach; ?>
-                </div>
-
-                <?php the_post_thumbnail('large-thumbnail', array('class' => '')); ?>
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php $categories = get_the_category(); ?>
+        <div class="text-center p-5 bg-yellow" style="margin-top: -15px;">
+            <h1 class="display-4 mb25"><span class="hash">#</span> <?php the_title(); ?></h1>
+            <div>
+                Category:
+                <?php foreach ($categories as $category) : ?>
+                    <a class="category" title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category) ?>"><?php echo $category->name; ?></a>
+                <?php endforeach; ?>
             </div>
-        <div class="post-content mt-4">
-            <?php the_content() ?>
-        </div>
 
-        <?php
-        if ( comments_open() || get_comments_number() ) {
-            comments_template();
-        }
-        ?>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <div class="container text-center">
-            <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+            <?php the_post_thumbnail('large-thumbnail', array('class' => '')); ?>
+        </div>
+        <div class="container">
+            <div class="post-content mt-4">
+                <?php the_content() ?>
+            </div>
+
+            <?php
+            if ( comments_open() || get_comments_number() ) {
+                comments_template();
+            }
+            ?>
+        </div>
+    <?php endwhile; ?>
+<?php else : ?>
+    <div class="text-center p-5 bg-yellow" style="margin-top: -15px;">
+        <h1 class="display-4 mb25"><span class="hash">#</span> Not Found</h1>
+    </div>
+    <div class="container">
+        <div class="post-content mt-4 text-center">
             <p>Whoops! The page that you were looking for, could not be found.</p>
             <p>Perhaps it got moved to another location, or no longer exists.</p>
             <a title="Back to the blog" class="btn btn-primary" href="<?php echo get_permalink(get_page_by_path('blog')); ?>">Back to the blog</a>
         </div>
-    <?php endif; ?>
-</div>
+    </div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
