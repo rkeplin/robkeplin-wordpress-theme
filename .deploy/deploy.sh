f2e4107b5558b851f1b957357413c405e6003144
Rob <rkeplin@gmail.com>
2019-10-22 11:32:44 -0400

    Calls automated script for plugin updates

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,13 +3,14 @@ openssl aes-256-cbc -K $encrypted_9112fb2807d4_key -iv $encrypted_9112fb2807d4_i
 
 chmod 0400 $(pwd)/.deploy/travis_id_rsa
 
-DATE_WITH_TIME=`date "+%Y-%m-%d_%H-%M-%S"`
-
-rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(pwd)/.deploy/travis_id_rsa" -avz --exclude '.deploy' --exclude '.git' $(pwd)/ travis@jersey2.rkeplin.com:/opt/apps/rkeplin-blog/application/wp-content/themes/robkeplin-$DATE_WITH_TIME
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com "chown -R travis:33 /opt/apps/rkeplin-blog/application/wp-content/themes/robkeplin-$DATE_WITH_TIME"
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com "cd /opt/apps/rkeplin-blog/application/wp-content/themes/ && ln -sfn robkeplin-$DATE_WITH_TIME robkeplin"
+rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(pwd)/.deploy/travis_id_rsa" -avz --exclude '.deploy' --exclude '.git' $(pwd)/ travis@jersey2.rkeplin.com:/opt/apps/rkeplin-blog/application/wp-content/themes/robkeplin-$TRAVIS_BUILD_NUMBER
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com "chown -R travis:33 /opt/apps/rkeplin-blog/application/wp-content/themes/robkeplin-$TRAVIS_BUILD_NUMBER"
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com "cd /opt/apps/rkeplin-blog/application/wp-content/themes/ && ln -sfn robkeplin-$TRAVIS_BUILD_NUMBER robkeplin"
 ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com "chown -h travis:33 /opt/apps/rkeplin-blog/application/wp-content/themes/robkeplin"
 
 ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'docker service update --force blog_rkeplin-blog'
+
+# Update plugins
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com 'rkplugin wordpress-seo 12.3'
 
 rm -rf $(pwd)/.deploy/travis_id_rsa
