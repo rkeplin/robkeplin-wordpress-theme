7d0a034ae7962e99d2b8d9a2b39b3a84fb10e678
Rob <rkeplin@gmail.com>
2019-12-04 14:49:02 -0500

    Update deploy.sh
    Updated wordfence plugin

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -13,6 +13,6 @@ ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1
 # Update plugins
 ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com 'rkplugin wordpress-seo 12.6.2'
 ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com 'rkplugin akismet 4.1.3'
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com 'rkplugin wordfence 7.4.1'
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey2.rkeplin.com 'rkplugin wordfence 7.4.2'
 
 rm -rf $(pwd)/.deploy/travis_id_rsa
